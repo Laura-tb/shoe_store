bca44e7ff0542071b6b925b59cf8ed7b699c63c1
Laura-tb <202150686+Laura-tb@users.noreply.github.com>
2025-11-17 13:36:20 +0100

    Estilos de formulario editar y crear usuario

--- app/views/UserUpdateView.php
+++ app/views/UserUpdateView.php
@@ -11,46 +11,57 @@ include('layout/head.php');
 
 
 <body>
-    <div class="page admin_users">
+    <div class="page login">
         <?php
         include('layout/navigation.php');
         ?>
 
         <main>
             <section class="hero">
-                <h1><?= $mode === 'create' ? 'Crear usuario' : 'Editar usuario' ?></h1>
-
-
                 <div class="container">
-                    <form method="POST">
-                        <label>Nombre:
-                            <input type="text" name="name" value="<?= $user['name'] ?>">
-                        </label>
+                    <section class="card">
 
-                        <label>Apellidos:
-                            <input type="text" name="surname" value="<?= $user['surname'] ?>">
-                        </label>
+                        <h1 class="title"><?= $mode === 'create' ? 'Crear usuario' : 'Editar usuario' ?></h1>
 
-                        <label>Email:
-                            <input type="email" name="email" value="<?= $user['email'] ?>">
-                        </label>
+                        <form method="POST">
+                            <div class="field">
+                                <label>Nombre:
+                                    <input type="text" name="name" value="<?= $user['name'] ?>">
+                                </label>
+                            </div>
 
-                        <label>Contraseña:
-                            <input type="text" name="pass_hash" value="<?= $user['pass_hash'] ?>">
-                        </label>
+                            <div class="field">
+                                <label>Apellidos:
+                                    <input type="text" name="surname" value="<?= $user['surname'] ?>">
+                                </label>
+                            </div>
 
-                        <label>Rol:
-                            <select name="role">
-                                <option value="client" <?= $user['role'] === 'client' ? 'selected' : '' ?>>Cliente</option>
-                                <option value="admin" <?= $user['role'] === 'admin' ? 'selected' : '' ?>>Admin</option>
-                            </select>
-                        </label>
+                            <div class="field">
+                                <label>Email:
+                                    <input type="email" name="email" value="<?= $user['email'] ?>">
+                                </label>
+                            </div>
 
-                        <button type="submit">
-                            <?= $mode === 'create' ? 'Crear' : 'Guardar cambios' ?>
-                        </button>
-                    </form>
+                            <div class="field">
+                                <label>Contraseña:
+                                    <input type="text" name="pass_hash" value="<?= $user['pass_hash'] ?>">
+                                </label>
+                            </div>
 
+                            <div class="field">
+                                <label>Rol:
+                                    <select name="role">
+                                        <option value="client" <?= $user['role'] === 'client' ? 'selected' : '' ?>>Cliente</option>
+                                        <option value="admin" <?= $user['role'] === 'admin' ? 'selected' : '' ?>>Admin</option>
+                                    </select>
+                                </label>
+                            </div>
+
+                            <button class="btn btn-primary btn-lg" type="submit">
+                                <?= $mode === 'create' ? 'Crear' : 'Guardar cambios' ?>
+                            </button>
+                        </form>
+                    </section>
                 </div>
             </section>
         </main>
