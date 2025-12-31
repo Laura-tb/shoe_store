606137cce40fc293f8e54e84e6ab67776df5d253
Laura-tb <202150686+Laura-tb@users.noreply.github.com>
2025-12-31 11:07:24 +0100

    Eliminar value en formulario

--- app/views/RegisterView.php
+++ app/views/RegisterView.php
@@ -24,26 +24,26 @@ include('../app/views/layout/head.php');
                         <form method="post" id="register-form" action="/clases_desarrollo_servidor/trabajo_enfoque/public/register-start.php" autocomplete="off" novalidate>
                             <div class="field">
                                 <label for="email">Email</label>
-                                <input id="email" name="email" type="email" placeholder="tuemail@email.com" value="random@gmail.com" />
+                                <input id="email" name="email" type="email" placeholder="tuemail@email.com" />
                                 <span class="error" id="emailError"></span>
                             </div>
 
                             <div class="field">
                                 <label for="name">Nombre</label>
-                                <input id="name" name="name" type="text" placeholder="Tu nombre" value="Random"/>
+                                <input id="name" name="name" type="text" placeholder="Tu nombre"/>
                                 <span class="error" id="nameError"></span>
                             </div>
 
                             <div class="field">
                                 <label for="surname">Apellidos</label>
-                                <input id="surname" name="surname" type="text" placeholder="Tus apellidos" value="Random" />
+                                <input id="surname" name="surname" type="text" placeholder="Tus apellidos" />
                             </div>
 
                             <div class="field">
                                 <label for="password">Contraseña</label>
                                 <div class="password">
                                     <input id="password" name="password" type="password" placeholder="••••••••"
-                                        title="Mínimo 8 caracteres con al menos una letra minúscula, una mayúscula, un carácter especial y un número." value="Random_1234" />
+                                        title="Mínimo 8 caracteres con al menos una letra minúscula, una mayúscula, un carácter especial y un número." />
                                     <span class="error" id="passwordError"></span>
                                 </div>
 
